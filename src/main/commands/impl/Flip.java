bac5598fb91917cbb0384edb32f9b74aba3a1bcc
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-01-06 21:51:47 -0800

    Update Flip.java

--- src/main/commands/impl/Flip.java
+++ src/main/commands/impl/Flip.java
@@ -1,6 +1,11 @@
 package main.commands.impl;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import main.Bot;
@@ -13,27 +18,45 @@ public class Flip extends Command
 	@Override
 	public void execute(Bot bot, MessageCreateEvent event)
 	{
-		// TODO Auto-generated method stub
 		Random random = new Random();
 		int num = random.nextInt(100);
+		Image choice;
 		if (num == 0)
 		{
-			
+			try
+			{
+				choice = ImageIO.read(new File("./assets/images/memes/sides.gif"));
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
 		else if (num % 2 == 1)
 		{
-			
+			try
+			{
+				choice = ImageIO.read(new File("./assets/images/memes/heads.jpg"));
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
 		else
 		{
-			
+			try
+			{
+				choice = ImageIO.read(new File("./assets/images/memes/tails.png"));
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
+		event.getMessage().getChannel().flatMap(channel->channel.createEmbed(spec))
 	}
 
 	@Override
 	public String description()
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
