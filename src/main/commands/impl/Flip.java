8b08ff82e6a7b1d572743d9b1a2eb9c6849ef71a
wttnguyen <59586133+wttnguyen@users.noreply.github.com>
2020-01-07 17:31:19 -0600

    Update Flip.java

--- src/main/commands/impl/Flip.java
+++ src/main/commands/impl/Flip.java
@@ -1,15 +1,6 @@
 package main.commands.impl;
 
-<<<<<<< HEAD
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
-=======
 import java.awt.Color;
->>>>>>> 69720ed01ad936b20a06d307a396bf4fe9dc2335
-
-import javax.imageio.ImageIO;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import main.Bot;
@@ -22,42 +13,7 @@ public class Flip extends Command
 	@Override
 	public Mono<Void> execute(Bot bot, MessageCreateEvent event)
 	{
-<<<<<<< HEAD
-		Random random = new Random();
-		int num = random.nextInt(100);
-		Image choice;
-		if (num == 0)
-		{
-			try
-			{
-				choice = ImageIO.read(new File("./assets/images/memes/sides.gif"));
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		else if (num % 2 == 1)
-		{
-			try
-			{
-				choice = ImageIO.read(new File("./assets/images/memes/heads.jpg"));
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		else
-		{
-			try
-			{
-				choice = ImageIO.read(new File("./assets/images/memes/tails.png"));
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		event.getMessage().getChannel().flatMap(channel->channel.createEmbed(spec))
-=======
+
 		double num = bot.getRandom().nextDouble();
 		return event.getMessage().getChannel().flatMap(channel -> channel.createEmbed(embed ->
 		{
@@ -66,13 +22,16 @@ public class Flip extends Command
 				embed.setImage("https://i.imgur.com/P3EbqRH.gif").setColor(Color.GREEN).setTitle("Sides");
 			} else if (num < 0.50)
 			{
-				embed.setImage("https://cdn.discordapp.com/attachments/602376454491078659/659905323217322021/image0.jpg").setColor(Color.ORANGE).setTitle("Heads");
+				embed.setImage(
+						"https://cdn.discordapp.com/attachments/602376454491078659/659905323217322021/image0.jpg")
+						.setColor(Color.ORANGE).setTitle("Heads");
 			} else
 			{
-				embed.setImage("https://cdn.discordapp.com/attachments/663534641164320801/663956797048225804/unknown.png").setColor(Color.BLUE).setTitle("Tails");
+				embed.setImage(
+						"https://cdn.discordapp.com/attachments/663534641164320801/663956797048225804/unknown.png")
+						.setColor(Color.BLUE).setTitle("Tails");
 			}
 		})).then();
->>>>>>> 69720ed01ad936b20a06d307a396bf4fe9dc2335
 	}
 
 	@Override
